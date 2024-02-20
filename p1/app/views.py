390abff2b85a953e5153188c72cbc1812d9fc7d8
utkarsh <utkarsh.agrawal.09@gmail.com>
2024-02-20 15:49:34 +0530

    2

--- p1/app/views.py
+++ p1/app/views.py
@@ -7,7 +7,7 @@ from .models import Booking
 from .serializers import BookingSerializer
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-from drf_yasg import openapi
+
 
 @swagger_auto_schema(method='GET', responses={200: BookingSerializer(many=True)})
 @api_view(['GET'])
