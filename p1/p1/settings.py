d39b3705443dc360b3790faa1670eb199d0326c9
utkarsh <utkarsh.agrawal.09@gmail.com>
2024-02-27 14:43:55 +0530

    a

--- p1/p1/settings.py
+++ p1/p1/settings.py
@@ -94,7 +94,7 @@ REDIS_POOL = redis.ConnectionPool(host='localhost', port=6379, db=0)
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Redis server location (change if needed)
+        'LOCATION': 'redis://65.2.177.148:6379/1',  # Redis server location (change if needed)
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'CONNECTION_POOL_KWARGS': {'max_connections': 100}
