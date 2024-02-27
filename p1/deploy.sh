667eae6cab087df587a666a1b1c89673e962502a
utkarsh <utkarsh.agrawal.09@gmail.com>
2024-02-27 12:45:00 +0530

    1

--- p1/deploy.sh
+++ p1/deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Check if nginx is installed
+if ! command -v nginx &> /dev/null; then
+    # If not installed, install nginx
+    sudo apt update
+    sudo apt install nginx -y
+fi
 
 sudo systemctl restart nginx
+
+# Check if virtual environment exists
+if [ ! -d "/home/ubuntu/myprojectenv" ]; then
+    # If not, create virtual environment
+    sudo -H pip3 install virtualenv
+    virtualenv /home/ubuntu/myprojectenv
+fi
+
 cd /home/ubuntu/
 source myprojectenv/bin/activate
 cd deployment/
