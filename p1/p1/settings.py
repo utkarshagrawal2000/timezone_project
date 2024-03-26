b2d1ba1f92dec34a5becb54081912baf92e6c35c
utkarsh <utkarsh.agrawal.09@gmail.com>
2024-03-26 17:36:04 +0530

    test

--- p1/p1/settings.py
+++ p1/p1/settings.py
@@ -163,9 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'static/'
+STATIC_ROOT='/home/ubuntu/deployment/static'
 
 
 # Default primary key field type
